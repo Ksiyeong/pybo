2dce923678e29d91e700f51cd9271993b48bed0b
Ksiyeong <yuiop2767@naver.com>
2022-08-16 21:42:17 +0900

    검색기능구현

--- main/views/base_views.py
+++ main/views/base_views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from ..models import Diary
+from django.db.models import Q
 
 
 
 def index(request):
     page = request.GET.get('page', '1') # 페이지를 읽어온다. 없을경우 1을 뱉는다 ?page =
+    kw = request.GET.get('kw', '') # 검색어
     diary_list = Diary.objects.order_by('-create_date')
+    if kw:
+        diary_list = diary_list.filter(
+            Q(subject__icontains=kw) | # 제목 검색
+            Q(content__icontains=kw) | # 내용 검색
+            Q(reply__content__icontains=kw) | # 댓글 내용
+            Q(reply__author__username__icontains=kw) | # 댓글 작성자
+            Q(author__username__icontains=kw) # 일기 작성자
+        ).distinct() # 중복제거
     paginator = Paginator(diary_list, 10) # 한페이지에 보여줄 페이지 갯수 10
     page_obj = paginator.get_page(page) # 페이지 오브젝트에 담아서 보여줌
-    context = {'diary_list' : page_obj}
+    context = {'diary_list' : page_obj, 'page': page, 'kw': kw}
     return render(request, 'main/diary_list.html', context)
 
 
